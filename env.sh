c563e08683a77c5fb234b3a928e45e39265762f8
Yaobin Wen <robin.wyb@gmail.com>
2024-03-01 23:11:09 +0000

    Make `env.sh` error out if not sourced in the `work-env` root directory

--- env.sh
+++ env.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-export ANSIBLE_ROLES_PATH="$HOME/.ansible/roles:/usr/share/ansible/roles:/etc/ansible/roles:$PWD/ansible/roles"
-export ANSIBLE_FILTER_PLUGINS="$HOME/.ansible/plugins/filter:/usr/share/ansible/plugins/filter:$PWD/ansible/filter_plugins"
-export ANSIBLE_LIBRARY="$HOME/.ansible/plugins/modules:/usr/share/ansible/plugins/modules:$PWD/ansible/library"
+test -e "./env.sh" || {
+  echo "ERROR: 'env.sh' is not in the current path. Are you in the 'work-env' root folder?"
+  return 1
+}
+
+export WORK_ENV_ROOT_DIR="$PWD"
+export ANSIBLE_ROLES_PATH="$HOME/.ansible/roles:/usr/share/ansible/roles:/etc/ansible/roles:$WORK_ENV_ROOT_DIR/ansible/roles"
+export ANSIBLE_FILTER_PLUGINS="$HOME/.ansible/plugins/filter:/usr/share/ansible/plugins/filter:$WORK_ENV_ROOT_DIR/ansible/filter_plugins"
+export ANSIBLE_LIBRARY="$HOME/.ansible/plugins/modules:/usr/share/ansible/plugins/modules:$WORK_ENV_ROOT_DIR/ansible/library"
