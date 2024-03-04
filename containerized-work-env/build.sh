7bb1244784ae48a4c4adf3e1ebc20d69c7aa921e
Yaobin Wen <robin.wyb@gmail.com>
2024-03-04 13:27:29 +0000

    Reformat build.sh

--- containerized-work-env/build.sh
+++ containerized-work-env/build.sh
@@ -2,96 +2,96 @@
 
 # Make sure the required tools are installed
 _check_required_tools() {
-  ansible-playbook --version || return
+    ansible-playbook --version || return
 }
 
 _clean_leftover() {
-  rm -f cid.stage-0 || return
-  rm -f inventory || return
-  rm -f work-env-image-id || return
+    rm -f cid.stage-0 || return
+    rm -f inventory || return
+    rm -f work-env-image-id || return
 }
 
 _main() {
-  local BASE_IMAGE
-  local WORK_ENV_NAME
-  local USER_NAME
-  local GROUP_NAME
-  local GIT_USER_EMAIL
-  local GIT_USER_NAME
-  local HOME_PATH_ON_HOST
+    local BASE_IMAGE
+    local WORK_ENV_NAME
+    local USER_NAME
+    local GROUP_NAME
+    local GIT_USER_EMAIL
+    local GIT_USER_NAME
+    local HOME_PATH_ON_HOST
 
-  BASE_IMAGE="$1"
-  WORK_ENV_NAME="$2"
-  USER_NAME="$3"
-  GROUP_NAME="$4"
-  GIT_USER_EMAIL="$5"
-  GIT_USER_NAME="$6"
-  HOME_PATH_ON_HOST="$7"
+    BASE_IMAGE="$1"
+    WORK_ENV_NAME="$2"
+    USER_NAME="$3"
+    GROUP_NAME="$4"
+    GIT_USER_EMAIL="$5"
+    GIT_USER_NAME="$6"
+    HOME_PATH_ON_HOST="$7"
 
-  IMAGE_STAGE_0="containerized-work-env-${WORK_ENV_NAME}.stage-0"
-  WORK_ENV_IMAGE="containerized-work-env-${WORK_ENV_NAME}"
-  IMAGE_VERSION="ubuntu-22.04"  # TODO(ywen): Do not hard-code.
+    IMAGE_STAGE_0="containerized-work-env-${WORK_ENV_NAME}.stage-0"
+    WORK_ENV_IMAGE="containerized-work-env-${WORK_ENV_NAME}"
+    IMAGE_VERSION="ubuntu-22.04"  # TODO(ywen): Do not hard-code.
 
-  docker image ls \
-    --filter "label=name=${WORK_ENV_IMAGE}" \
-    --filter "label=version=${IMAGE_VERSION}" \
-    --format "{{.ID}}" > work-env-image-id || return
+    docker image ls \
+        --filter "label=name=${WORK_ENV_IMAGE}" \
+        --filter "label=version=${IMAGE_VERSION}" \
+        --format "{{.ID}}" > work-env-image-id || return
 
-  WORK_ENV_IMAGE_ID=
-  if [ -e work-env-image-id ]; then
-    WORK_ENV_IMAGE_ID=$(cat work-env-image-id) || return
-  fi
+    WORK_ENV_IMAGE_ID=
+    if [ -e work-env-image-id ]; then
+        WORK_ENV_IMAGE_ID=$(cat work-env-image-id) || return
+    fi
 
-  if [ -z "$WORK_ENV_IMAGE_ID" ]; then
-    echo "The Docker image '${WORK_ENV_IMAGE}:${IMAGE_VERSION}' does not exist. Creating it..." || return
+    if [ -z "$WORK_ENV_IMAGE_ID" ]; then
+        echo "The Docker image '${WORK_ENV_IMAGE}:${IMAGE_VERSION}' does not exist. Creating it..." || return
 
-    # The work env image does not exist yet so we need to create it.
-    _check_required_tools || return
+        # The work env image does not exist yet so we need to create it.
+        _check_required_tools || return
 
-    _clean_leftover || return
+        _clean_leftover || return
 
-    # Build image stage 0.
-    docker build \
-        --file Dockerfile.0 \
-        --tag "${IMAGE_STAGE_0}:${IMAGE_VERSION}" \
-        --label "name=${WORK_ENV_IMAGE}" \
-        --label "version=${IMAGE_VERSION}" \
-        --build-arg BASE_IMAGE="${BASE_IMAGE}" \
-        --build-arg WORK_ENV_NAME="${WORK_ENV_NAME}" \
-        --build-arg USER_NAME="${USER_NAME}" \
-        --build-arg GROUP_NAME="${GROUP_NAME}" \
-        --build-arg GIT_USER_EMAIL="${GIT_USER_EMAIL}" \
-        --build-arg GIT_USER_NAME="${GIT_USER_NAME}" \
-        .  || return
+        # Build image stage 0.
+        docker build \
+            --file Dockerfile.0 \
+            --tag "${IMAGE_STAGE_0}:${IMAGE_VERSION}" \
+            --label "name=${WORK_ENV_IMAGE}" \
+            --label "version=${IMAGE_VERSION}" \
+            --build-arg BASE_IMAGE="${BASE_IMAGE}" \
+            --build-arg WORK_ENV_NAME="${WORK_ENV_NAME}" \
+            --build-arg USER_NAME="${USER_NAME}" \
+            --build-arg GROUP_NAME="${GROUP_NAME}" \
+            --build-arg GIT_USER_EMAIL="${GIT_USER_EMAIL}" \
+            --build-arg GIT_USER_NAME="${GIT_USER_NAME}" \
+            .  || return
 
-    # Start stage 0 container for further configuration.
-    docker run -d \
-        --name "${IMAGE_STAGE_0}" \
-        --cidfile cid.stage-0 \
-        --tmpfs /tmp:exec \
-        -v /etc/localtime:/etc/localtime:ro \
-        "${IMAGE_STAGE_0}:${IMAGE_VERSION}" \
-            /bin/sh -c 'while sleep 600; do :; done' || return
+        # Start stage 0 container for further configuration.
+        docker run -d \
+            --name "${IMAGE_STAGE_0}" \
+            --cidfile cid.stage-0 \
+            --tmpfs /tmp:exec \
+            -v /etc/localtime:/etc/localtime:ro \
+            "${IMAGE_STAGE_0}:${IMAGE_VERSION}" \
+                /bin/sh -c 'while sleep 600; do :; done' || return
 
-      # Run the Ansible provisioner.
-      CID="$(cat "cid.stage-0")" || return
-      echo "$CID ansible_user=root ansible_python_interpreter=auto" \
-          >"inventory" || return
+        # Run the Ansible provisioner.
+        CID="$(cat "cid.stage-0")" || return
+        echo "$CID ansible_user=root ansible_python_interpreter=auto" \
+            >"inventory" || return
 
-      ansible-playbook -vv -c docker -i "inventory" \
-        -e "git_user_email='$GIT_USER_EMAIL'" \
-        -e "git_user_full_name='$GIT_USER_NAME'" \
-        -e "unprivileged_user_name='$USER_NAME'" \
-        "containerized-work-env.yml" || return
+        ansible-playbook -vv -c docker -i "inventory" \
+            -e "git_user_email='$GIT_USER_EMAIL'" \
+            -e "git_user_full_name='$GIT_USER_NAME'" \
+            -e "unprivileged_user_name='$USER_NAME'" \
+            "containerized-work-env.yml" || return
 
-      docker stop "$CID" || return
+        docker stop "$CID" || return
 
-      docker commit "$CID" "${WORK_ENV_IMAGE}:${IMAGE_VERSION}" || return
+        docker commit "$CID" "${WORK_ENV_IMAGE}:${IMAGE_VERSION}" || return
 
-      docker image rm -f "${IMAGE_STAGE_0}:${IMAGE_VERSION}" || return
-  else
-    echo "The Docker image '${WORK_ENV_IMAGE}:${IMAGE_VERSION}' already exists." || return
-  fi
+        docker image rm -f "${IMAGE_STAGE_0}:${IMAGE_VERSION}" || return
+    else
+        echo "The Docker image '${WORK_ENV_IMAGE}:${IMAGE_VERSION}' already exists." || return
+    fi
 
     # Configure the host system.
     cd "${WORK_ENV_ROOT_DIR}/ansible" || return
@@ -139,55 +139,55 @@ HOME_PATH_ON_HOST="$HOME_PATH_ON_HOST_DEFAULT"
 
 # Process the options and arguments
 while true; do
-  case "$1" in
-    --base-image)
-      echo "Option --base-image: $2"
-      BASE_IMAGE="$2"
-      shift 2
-      ;;
-    --work-env-name)
-      echo "Option --work-env-name: $2"
-      WORK_ENV_NAME="$2"
-      shift 2
-      ;;
-    --user-name)
-      echo "Option --user-name: $2"
-      USER_NAME="$2"
-      shift 2
-      ;;
-    --group-name)
-      echo "Option --group-name: $2"
-      GROUP_NAME="$2"
-      shift 2
-      ;;
-    --git-user-email)
-      echo "Option --git-user-email: $2"
-      GIT_USER_EMAIL="$2"
-      shift 2
-      ;;
-    --git-user-name)
-      echo "Option --git-user-name: $2"
-      GIT_USER_NAME="$2"
-      shift 2
-      ;;
-    --home-path-on-host)
-      echo "Option --home-path-on-host: $2"
-      HOME_PATH_ON_HOST="$2"
-      shift 2
-      ;;
-    --help)
-      echo "$USAGE"
-      exit 0
-      ;;
-    --)
-      shift
-      break
-      ;;
-    *)
-      echo "Unknown option: $1"
-      exit 1
-      ;;
-  esac
+    case "$1" in
+        --base-image)
+        echo "Option --base-image: $2"
+        BASE_IMAGE="$2"
+        shift 2
+        ;;
+        --work-env-name)
+        echo "Option --work-env-name: $2"
+        WORK_ENV_NAME="$2"
+        shift 2
+        ;;
+        --user-name)
+        echo "Option --user-name: $2"
+        USER_NAME="$2"
+        shift 2
+        ;;
+        --group-name)
+        echo "Option --group-name: $2"
+        GROUP_NAME="$2"
+        shift 2
+        ;;
+        --git-user-email)
+        echo "Option --git-user-email: $2"
+        GIT_USER_EMAIL="$2"
+        shift 2
+        ;;
+        --git-user-name)
+        echo "Option --git-user-name: $2"
+        GIT_USER_NAME="$2"
+        shift 2
+        ;;
+        --home-path-on-host)
+        echo "Option --home-path-on-host: $2"
+        HOME_PATH_ON_HOST="$2"
+        shift 2
+        ;;
+        --help)
+        echo "$USAGE"
+        exit 0
+        ;;
+        --)
+        shift
+        break
+        ;;
+        *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
 done
 
 _main "$BASE_IMAGE" "$WORK_ENV_NAME" "$USER_NAME" "$GROUP_NAME" "$GIT_USER_EMAIL" "$GIT_USER_NAME" "$HOME_PATH_ON_HOST"
